ab15162b0a832080faf2eb7b2849f403a4e3ecb0
Everton P. Cruz <kyry.ix@gmail.com>
2019-11-29 18:21:47 -0300

    Modificacoes em tela de ajuda e atualizacao no suporte a relatorio no executavel

--- windows/windowhelp.cpp
+++ windows/windowhelp.cpp
@@ -4,6 +4,8 @@
 #include <QDir>
 #include <stdio.h>
 #include <QMessageBox>
+#include <stdlib.h>
+#include <stdio.h>
 
 WindowHelp::WindowHelp(QWidget *parent) :
     QDialog(parent),
@@ -11,7 +13,8 @@ WindowHelp::WindowHelp(QWidget *parent) :
 {
     ui->setupUi(this);
     std::string path = QDir::currentPath().toStdString() + "/debug/doc/index.html";
-    FILE* fp = fopen( path.c_str(), "r" );
+    //FILE* fp = fopen( path.c_str(), "r,ccs=UTF-8" ); // c++11
+    FILE* fp = fopen( path.c_str(), "r" ); // have ansi
     if( fp == nullptr ){
         QMessageBox msgBox;
         std::string information = "Erro na abertura do arquivo.";
@@ -27,7 +30,7 @@ WindowHelp::WindowHelp(QWidget *parent) :
     long size = ftell( fp );
     fseek( fp, 0, SEEK_SET );
 
-    char* buffer = static_cast<char*>( malloc( static_cast<unsigned long>(size) * sizeof(char) ) );
+    char* buffer = static_cast<char*>( malloc( static_cast<size_t>(size) * sizeof(char) ) );
     if( buffer == nullptr ){
         QMessageBox msgBox;
         msgBox.setInformativeText( "Erro na alocacao de memoria." );
@@ -40,12 +43,10 @@ WindowHelp::WindowHelp(QWidget *parent) :
     }
 
     size_t result = fread( buffer, sizeof(char), static_cast<size_t>(size), fp );
-    if( static_cast<long>(result) != size ){
+    //if( static_cast<long>(result) != size ){
+    if( result == 0 ){
         QMessageBox msgBox;
-        std::string str = "Erro na leitura das informacoes.\nresult=";
-        str += std::to_string( result );
-        str += " e size=";
-        str += std::to_string( size );
+        std::string str = "Erro na leitura das informacoes.";
         msgBox.setInformativeText( str.c_str() );
         msgBox.setIcon( QMessageBox::Warning );
         msgBox.setStandardButtons( QMessageBox::Ok );
@@ -55,7 +56,26 @@ WindowHelp::WindowHelp(QWidget *parent) :
         return;
     }
 
-    ui->textBrowser->setHtml( buffer );
+    std::string html( buffer );
+    std::string str( "src=\"./" );
+
+    std::size_t found = html.find( str );
+    std::string replace = "";
+    while( found != std::string::npos ){
+        replace = "src=\"";
+        replace += QDir::currentPath().toStdString();
+        replace += "/debug/doc/";
+        html.replace( found, 7, replace );
+        found = html.find( str, found );
+    }
+
+    std::string path2 = path + ".html";
+    FILE* fp2 = fopen( path2.c_str(), "w" );
+    fwrite( html.c_str(), 1, static_cast<size_t>(html.size()), fp2 );
+    fclose(fp2);
+
+    //ui->textBrowser->setHtml( buffer );
+    ui->textBrowser->setHtml( html.c_str() );
 
     fclose( fp );
     free( buffer );
