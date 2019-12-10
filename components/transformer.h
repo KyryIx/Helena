dd4bb96488f72c55f9881ea910fd1d5f465afbcb
Everton P. Cruz <kyry.ix@gmail.com>
2019-12-10 12:40:38 -0200

    Atualizacao no software, correcao da exportacao da folha de dados e insercao do termo gerenciador nos icones de materiais

--- components/transformer.h
+++ components/transformer.h
@@ -1282,28 +1282,30 @@ class Transformer{
         std::string toHTML() const{
             std::string txt = "";
             txt = txt + "<table align=\"center\" class=\"transformer\" width=\"100%\">\n";
+            txt = txt + "\t<tr><td colspan=\"2\"><h1 align=\"center\">Datasheet</h1></td></tr>\n";
+            txt = txt + "\t<tr><td>&nbsp;</td></tr>\n";
             txt = txt + "\t<tr><td align=\"center\" colspan=\"2\" style=\"background-color:#ddd;\">GENERAL INFORMATION</td></tr>\n";
             txt = txt + "\t<tr><td align=\"right\" width=\"50%\">Transformer with: "
                     + std::to_string( this->getTransformerPatternNumber() ) + " ("
                     + this->getTransformerPatternNameAuto()  + ")</td><td><b>";
 
             txt = txt + "</td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"background-color:#f00;heght:100px;\">Frequency:</td><td><b>" + std::to_string( this->getFrequency() ) + " Hz</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Magnetic Induction:</td><td><b>" + std::to_string( this->getMagneticInduction() ) + " G</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Current Density:</td><td><b>" + std::to_string( this->getCurrentDensity() ) + " A/mm<sup>2</sup></b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Average Cur. Density:</td><td><b>" + std::to_string( this->getAverageCurrentDensity() ) + " A/mm<sup>2</sup></b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Weigth Iron:</td><td><b>" + std::to_string( this->getWeightIron()*1000.0 ) + " g</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Weight Copper:</td><td><b>" + std::to_string( this->getWeightCopper()*1000.0 ) + " g</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Turns Average Length:</td><td><b>" + std::to_string( this->getAverageTurnLength()/10 ) + " cm</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Coil Area:</td><td><b>" + std::to_string( this->getCopperArea() ) + " mm<sup>2</sup></b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Iron Loss:</td><td><b>" + std::to_string( this->getIronLoss() ) + " W</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Copper Loss:</td><td><b>" + std::to_string( this->getCopperLoss() ) + " W</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Total Loss:</td><td><b>" + std::to_string( this->getTotalLoss() ) + " W</b></td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\" style=\"50\">Efficiency:</td><td><b>" + std::to_string( this->getEfficiency() ) + " %</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Frequency:</td><td><b>" + std::to_string( this->getFrequency() ) + " Hz</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Magnetic Induction:</td><td><b>" + std::to_string( this->getMagneticInduction() ) + " G</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Current Density:</td><td><b>" + std::to_string( this->getCurrentDensity() ) + " A/mm<sup>2</sup></b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Average Cur. Density:</td><td><b>" + std::to_string( this->getAverageCurrentDensity() ) + " A/mm<sup>2</sup></b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Weigth Iron:</td><td><b>" + std::to_string( this->getWeightIron()*1000.0 ) + " g</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Weight Copper:</td><td><b>" + std::to_string( this->getWeightCopper()*1000.0 ) + " g</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Turns Average Length:</td><td><b>" + std::to_string( this->getAverageTurnLength()/10 ) + " cm</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Coil Area:</td><td><b>" + std::to_string( this->getCopperArea() ) + " mm<sup>2</sup></b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Iron Loss:</td><td><b>" + std::to_string( this->getIronLoss() ) + " W</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Copper Loss:</td><td><b>" + std::to_string( this->getCopperLoss() ) + " W</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Total Loss:</td><td><b>" + std::to_string( this->getTotalLoss() ) + " W</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Efficiency:</td><td><b>" + std::to_string( this->getEfficiency() ) + " %</b></td></tr>\n";
             txt = txt + "\t<tr><td colspan=\"2\">&nbsp;</td></tr>\n";
 
             txt = txt + "\t<tr><td align=\"center\" colspan=\"2\" style=\"background-color:#ddd;\">INPUT</td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\">Power:</td><td><b>" + std::to_string( this->getInputPower() ) + " W</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Power:</td><td><b>" + std::to_string( this->getInputPower() ) + " VA</b></td></tr>\n";
             txt = txt + "\t<tr><td colspan=\"2\">&nbsp;</td></tr>\n";
             txt = txt + "\t<tr><td align=\"right\">Voltage 1:</td><td><b>" + std::to_string( this->getInputVoltage1() ) + " V</b></td></tr>\n";
             txt = txt + "\t<tr><td align=\"right\">Current 1:</td><td><b>" + std::to_string( this->getInputCurrent1() ) + " A</b></td></tr>\n";
@@ -1332,7 +1334,7 @@ class Transformer{
             }
 
             txt = txt + "\t<tr><td align=\"center\" colspan=\"2\" style=\"background-color:#ddd;\">OUTPUT</td></tr>\n";
-            txt = txt + "\t<tr><td align=\"right\">Power:</td><td><b>" + std::to_string( this->getOutputPower() ) + " W</b></td></tr>\n";
+            txt = txt + "\t<tr><td align=\"right\">Power:</td><td><b>" + std::to_string( this->getOutputPower() ) + " VA</b></td></tr>\n";
             txt = txt + "\t<tr><td colspan=\"2\">&nbsp;</td></tr>\n";
             txt = txt + "\t<tr><td align=\"right\">Voltage 1:</td><td><b>" + std::to_string( this->getOutputVoltage1() ) + " V</b></td></tr>\n";
             txt = txt + "\t<tr><td align=\"right\">Current 1:</td><td><b>" + std::to_string( this->getOutputCurrent1() ) + " A</b></td></tr>\n";
