35913a404cf27353b2a9fa3cec22dfb0630f2be3
Everton P. Cruz <kyry.ix@gmail.com>
2020-01-20 17:54:31 -0300

    Atualizacao do calculo de densidade da lamina e insercao dos valores para a base de dados

--- windows/windowlaminaimage.cpp
+++ windows/windowlaminaimage.cpp
@@ -57,22 +57,92 @@ void WindowLaminaImage::on_pushButton_calculate_clicked(){
     area -= 2 * pi * pow( J, 2.0 ) / 4.0;
     area -= 2 * pi * pow( K, 2.0 ) / 4.0;
 
-    // density material - lamina //
+    // density material (g/cm^3) - lamina //
     double materialDensity = ui->lineEdit_materialDensity->text().toDouble();
-    // tickness lamina - lamina //
+    // density lamina (kg/cm) //
+    double densityLamina = (area * materialDensity) / 100000.0;
+    ui->lineEdit_weight->setText( std::to_string(densityLamina).c_str() );
+
+    // tickness lamina (mm) - lamina //
     double tickenessLamina = ui->lineEdit_thicknessLamina->text().toDouble();
-    // thickness layer - layer //
+    // thickness layer (mm) - layer //
     double ticknessLayer = ui->lineEdit_thicknessLayer->text().toDouble();
+    // % //
+    double compensation = (2.0 * ticknessLayer) / (tickenessLamina * 1000.0);
+    ui->lineEdit_compensationPercent->setText( std::to_string(compensation).c_str() );
+}
 
-    double tickenessLaminaReal = tickenessLamina * 1000.0 - 2 * ticknessLayer;
-    double areaCm2 = area / 100.0;
+void WindowLaminaImage::clearFields(){
+    ui->lineEdit_A->clear();
+    ui->lineEdit_B->clear();
+    ui->lineEdit_C->clear();
+    ui->lineEdit_D->clear();
+    ui->lineEdit_E->clear();
+    ui->lineEdit_F->clear();
+    ui->lineEdit_G->clear();
+    ui->lineEdit_H->clear();
+    ui->lineEdit_I->clear();
+    ui->lineEdit_J->clear();
+    ui->lineEdit_K->clear();
 
+    ui->lineEdit_code->clear();
+    ui->lineEdit_loss->clear();
+    ui->lineEdit_type->clear();
+    ui->lineEdit_width->clear();
+    ui->lineEdit_weight->clear();
+    ui->lineEdit_provider->clear();
+    ui->lineEdit_windowArea->clear();
+    ui->lineEdit_stackingFactor->clear();
+    ui->lineEdit_thicknessLayer->clear();
+    ui->lineEdit_materialDensity->clear();
+    ui->lineEdit_thicknessLamina->clear();
+    ui->lineEdit_compensationPercent->clear();
+}
 
-    // falta o processo de transformar a massa por centimetro
+void WindowLaminaImage::on_pushButton_clear_clicked(){
+    QMessageBox msgBox;
+    msgBox.setInformativeText( "Deseja realmente limpar os campos?" );
+    msgBox.setIcon( QMessageBox::Warning );
+    msgBox.setStandardButtons( QMessageBox::Ok|QMessageBox::No );
+    if( msgBox.exec() == QMessageBox::Ok ){
+        this->clearFields();
+    }
+}
 
+void WindowLaminaImage::on_pushButton_insert_clicked(){
+    QMessageBox msgBox;
+    msgBox.setInformativeText( "Deseja salvar mesmo?" );
+    msgBox.setIcon( QMessageBox::Warning );
+    msgBox.setStandardButtons( QMessageBox::Ok|QMessageBox::No );
+    if( msgBox.exec() == QMessageBox::Ok ){
+        Lamina* lamina = new Lamina();
+        //lamina->setId( ui->lineEdit_id->text().toUInt() );
+        lamina->setType( ui->lineEdit_type->text().toStdString() );
+        lamina->setProvider( ui->lineEdit_provider->text().toStdString() );
+        lamina->setWidth( ui->lineEdit_width->text().toDouble() );
+        lamina->setWindowArea( ui->lineEdit_windowArea->text().toDouble() );
+        lamina->setWeight( ui->lineEdit_weight->text().toDouble() );
+        lamina->setCompensationPercent( ui->lineEdit_compensationPercent->text().toDouble() );
 
+        if( this->database->executeSQL( lamina->toSQL() ) > -1 ){
+            msgBox.setInformativeText( "Salvamento feito com sucesso." );
+            msgBox.setIcon( QMessageBox::Information );
+        }
+        else{
+            msgBox.setInformativeText( "Erro na consulta." );
+            msgBox.setIcon( QMessageBox::Warning );
+        }
+        msgBox.setStandardButtons( QMessageBox::Ok );
+        msgBox.exec();
+    }
 }
 
 void WindowLaminaImage::on_pushButton_exit_clicked(){
-    this->close();
+    QMessageBox msgBox;
+    msgBox.setInformativeText( "Deseja realmente sair do formulário?" );
+    msgBox.setIcon( QMessageBox::Warning );
+    msgBox.setStandardButtons( QMessageBox::Ok|QMessageBox::No );
+    if( msgBox.exec() == QMessageBox::Ok ){
+       this->close();
+    }
 }
